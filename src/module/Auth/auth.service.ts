c49d7da343ea5432d07b575777b896a0b1cdc117
mehadishisir <mehadishisir@gmail.com>
2026-09-09 22:05:11 +0600

    add register user to db

--- src/module/Auth/auth.service.ts
+++ src/module/Auth/auth.service.ts
@@ -2,6 +2,9 @@ import { prisma } from "../../lib/prisma";
 import { AppError } from "../../utils/AppError";
 import { IRegisterPayload } from "./auth.interface";
 import httpStatus from "http-status"
+import bcrypt from "bcryptjs"
+import config from "../../config";
+import { UserRole } from "../../generated/prisma/enums";
 const registrationUser= async (payload:IRegisterPayload)=>{
     const {name,email,password,phone}=payload
 
@@ -13,4 +16,18 @@ const registrationUser= async (payload:IRegisterPayload)=>{
     if (existingUser){
         throw new AppError(httpStatus.CONFLICT, "User already exists with this email")
     }
+
+    const hashPassword = await bcrypt.hash(password,Number(config.bcrypt_salt_rounds))
+
+    const user = await prisma.user.create({
+        data:{
+            name,
+            email,
+            password:hashPassword,
+            phone,
+            role:UserRole.PATIENT
+        }
+    })
+    return user
+
 }
