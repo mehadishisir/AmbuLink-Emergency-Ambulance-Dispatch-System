18dba6ee32c842e5a3706d6b2d6db0335b94a033
Mehadi Shisir <mehadishisir@gmail.com>
2026-09-21 21:44:22 +0600

    feat(ambulance): update payload interfaces to use enums for type and status fields

--- src/module/Ambulance/ambulance.interface.ts
+++ src/module/Ambulance/ambulance.interface.ts
@@ -1,27 +1,38 @@
+import {
+  AmbulanceType,
+  AmbulanceStatus,
+} from "../../generated/prisma/enums";
+
+
+// Create Ambulance Payload
 export interface ICreateAmbulancePayload {
   vehicleNumber: string;
   model: string;
-  type: string;
-  status: string;
+  type: AmbulanceType;
+  status: AmbulanceStatus;
   latitude: number;
   longitude: number;
   driverId?: string | null;
 }
 
+
+// Update Ambulance Payload
 export interface IUpdateAmbulancePayload {
   vehicleNumber?: string;
   model?: string;
-  type?: string;
-  status?: string;
+  type?: AmbulanceType;
+  status?: AmbulanceStatus;
   latitude?: number;
   longitude?: number;
   driverId?: string | null;
 }
 
+
+// Ambulance Filter Query
 export interface IAmbulanceFilterQuery {
   page?: string;
   limit?: string;
   search?: string;
-  status?: string;
-  type?: string;
+  status?: AmbulanceStatus;
+  type?: AmbulanceType;
 }
